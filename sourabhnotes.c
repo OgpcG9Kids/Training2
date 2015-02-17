b9c43640ffcf7dc144d3b378ae317f92dd01822f
Neel Balusa <neelbalusa@gmail.com>
2015-02-16 21:03:57 -0800

    changed

--- sourabhnotes.c
+++ sourabhnotes.c
@@ -1,4 +1,6 @@
 void main()
 {
-	print("Hello I am stupid Sourabh and i like bball but i kinda dont know how to spell it^^^");
+	printf("Hello I am stupid Sourabh and i like bball but i kinda dont know how to spell it^^^");
+	printf("My favorite pet is Tiger");
+	
 }
