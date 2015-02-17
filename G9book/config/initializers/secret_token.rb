70fb840c6e88d0206b78699d8604712a450582e1
Neel Balusa <neelbalusa@gmail.com>
2015-02-16 18:45:04 -0800

    added motivation

--- G9book/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-G9book::Application.config.secret_token = '2c3eef6cf154b3afcef9b0025607c675b8d2f84ec10fd16a3e3298e03f31823a15a7c4927897f21a9cd6d10612085c38c547ac123ed748568b01127a077a95d2'
